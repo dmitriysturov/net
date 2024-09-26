4d91b7a77c72f2a4058b4fc3fac62d9efac821ab
dmitriysturov <sturovdmitriyal@gmail.com>
2024-09-27 03:04:31 +0500

    lab3 initial

--- labs/obj/Debug/net8.0/labs.AssemblyInfo.cs
+++ labs/obj/Debug/net8.0/labs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("labs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b488ada8b3b2165b1e6181e03cbfdf9f07925db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99ff02b88ab2e4f56e97bf8e45c9556cae44d7e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("labs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("labs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
