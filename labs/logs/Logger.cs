9269885d1dd756876a13a3d89c3692c09f66436d
dmitriysturov <sturovdmitriyal@gmail.com>
2024-09-27 06:06:31 +0500

    lab5 completed

--- labs/logs/Logger.cs
+++ labs/logs/Logger.cs
@@ -14,35 +14,4 @@ namespace PcTechs.logs
             LogEvent?.Invoke(logMessage);
         }
     }
-
-
-    public class FileLogger
-    {
-        private string _filePath;
-
-        public FileLogger(string filePath)
-        {
-            // Проверяем и создаём директорию, если она не существует
-            string directoryPath = Path.GetDirectoryName(filePath);
-            if (!Directory.Exists(directoryPath))
-            {
-                Directory.CreateDirectory(directoryPath);  // Создаём директорию, если она не существует
-            }
-
-            _filePath = filePath;
-        }
-
-        // Метод для записи лога в файл
-        public void PrintToFile(string message)
-        {
-            using (StreamWriter writer = new StreamWriter(_filePath, true))
-            {
-                writer.WriteLine(message);
-            }
-        }
-    }
-
-    
-
-    
 }
